79d879b897ef43398efe7f7d9887260693437f70
Omnironan <rshortall@omnifone.com>
2014-05-09 15:07:37 +0100

    Renamed ClassLibrary to SharedViewModels
    Added reference to MVVM Light PCL to both Win8.1 and WinP8.1

---
+++ ClassLibrary1/SharedViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GalaSoft.MvvmLight;
+
+namespace SharedViewModels
+{
+    public class SharedViewModel : ViewModelBase
+    {
+        public SharedViewModel()
+        {
+            TestProperty = IsInDesignMode
+                ? "This Test Property from Design Mode"
+                : "This Test Property from Runtime Mode";
+        }
+
+        private string _testProperty;
+
+        public string TestProperty
+        {
+            get { return _testProperty; }
+            set
+            {
+                _testProperty = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
---
+++ ClassLibrary1/ViewModelLocator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace SharedViewModels
+{
+    public class ViewModelLocator
+    {
+        public SharedViewModel SharedViewModel
+        {
+            get { return ServiceLocator.Current.GetInstance<SharedViewModel>(); }
+        }
+
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            SimpleIoc.Default.Register<SharedViewModel>();
+        }
+    }
+}
